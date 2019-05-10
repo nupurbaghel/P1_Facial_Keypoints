3ecb51bbd4b6db8b769bb32860cb641c50d3b084
nupur <nupurbaghel@gmail.com>
2019-05-11 00:31:16 +0530

    Adding solutions for project

--- models.py
+++ models.py
@@ -20,7 +20,23 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        # input size is 224 x 224
         self.conv1 = nn.Conv2d(1, 32, 5)
+        # output size is (224-5)/1 + 1 = 220, ie (32, 220, 220)
+        # after pool (32, 110, 110)
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # output size is (110-3)/1+1 = 108, ie (64, 108, 108)
+        # after pool (64, 54, 54)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        
+        # output size after conv3 is (54-3)/1 + 1 = 52, ie (128, 52, 52)
+        # after pool (128, 26, 26)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        
+        self.fc1 = nn.Linear(128*26*26, 1000)
+        self.fc2 = nn.Linear(1000, 500)
+        self.fc3 = nn.Linear(500, 136)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -30,8 +46,13 @@ class Net(nn.Module):
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
+        x = F.dropout(self.pool(F.relu(self.conv1(x))),0.1)
+        x = F.dropout(self.pool(F.relu(self.conv2(x))),0.2)
+        x = F.dropout(self.pool(F.relu(self.conv3(x))),0.3)
+        x = x.view(x.shape[0],-1)
+        x = F.dropout(F.relu(self.fc1(x)),0.2)
+        x = F.dropout(F.relu(self.fc2(x)),0.3)
+        x = self.fc3(x)
+
         # a modified x, having gone through all the layers of your model, should be returned
         return x
